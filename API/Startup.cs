bd23e8d017917411d69b037b7b9f1facb13cf484
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-09-27 15:28:15 +0300

    created initial migration and added to program.cs to apply migrations

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,8 +18,6 @@ namespace API
             _config = config;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -34,7 +32,7 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
-        
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
