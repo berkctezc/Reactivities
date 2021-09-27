a7148c2364c987efc16852638a53aca5a62ef20a
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-09-27 17:47:01 +0300

    added sample controller for activities

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivities(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+
+    }
+}
