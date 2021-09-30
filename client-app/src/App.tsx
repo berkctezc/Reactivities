bbd9183c7076eeb22077e830f3f8530f145a0aa5
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-09-30 14:36:17 +0300

    semantic ui demo

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+import {Header, List} from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -14,18 +15,15 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <ul>
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+        <List>
           {activities.map((activity:any)=>(
-            <li key={activity.id}>
+            <List.Item key={activity.id}>
               {activity.title}
-            </li>
+            </List.Item>
           ))}
-        </ul>
-      </header>
+        </List>
     </div>
   );
 }
