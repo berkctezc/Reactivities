a392ac55f06305fdad4b281f4dfeb4682eb0b107
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-10-01 11:43:45 +0300

    activity details

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Card, Image, Button } from 'semantic-ui-react';
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+    activity: Activity
+}
+
+export default function ActivityDetails({ activity }: Props) {
+    return (
+        <Card fluid>
+            <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+            <Card.Content>
+                <Card.Header>
+                    {activity.title}
+                </Card.Header>
+                <Card.Meta>
+                    <span>{activity.date}</span>
+                </Card.Meta>
+                <Card.Description>
+                    {activity.description}
+                </Card.Description>
+            </Card.Content>
+            <Card.Content>
+                <Button.Group widths='2'>
+                    <Button basic color='blue' content='edit' />
+                    <Button basic color='grey' content='cancel' />
+                </Button.Group>
+            </Card.Content>
+        </Card>
+    )
+}
