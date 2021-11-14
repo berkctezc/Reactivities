86a83809234931f37fffa57829c84760d18151b9
berkctezc <berkcantezcaner@gmail.com>
2021-11-14 21:52:50 +0300

    posting data to server

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from "axios";
-import { Activity } from "../models/activity";
+import { Activity } from '../models/activity';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -24,13 +24,17 @@ const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
     get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-    post: <T>(url: string) => axios.post<T>(url).then(responseBody),
-    put: <T>(url: string) => axios.put<T>(url).then(responseBody),
+    post: <T>(url: string, body: T) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: T) => axios.put<T>(url, body).then(responseBody),
     del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
 
 const Activities = {
     list: () => requests.get<Activity[]>("/activities"),
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => requests.post<Activity>('/activities', activity),
+    update: (activity: Activity) => requests.put<Activity>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => axios.delete<Activity>(`/activities/${id}`)
 };
 
 const agent = {
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,9 +14,10 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: (boolean);
 }
 
-export default function ActivityDashboard({ activities, selectedActivity, selectActivity, deleteActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit }: Props) {
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, deleteActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, submitting }: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -38,7 +39,12 @@ export default function ActivityDashboard({ activities, selectedActivity, select
                     />
                 }
                 {editMode &&
-                    <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />
+                    <ActivityForm
+                        closeForm={closeForm}
+                        activity={selectedActivity}
+                        createOrEdit={createOrEdit}
+                        submitting={submitting}
+                    />
                 }
             </Grid.Column>
         </Grid>
