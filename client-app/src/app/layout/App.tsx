b6dd4207c92d7b1c8d931b6f2efd4b23e97cdbdd
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-12-17 15:54:40 +0300

    added routing

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,25 +1,21 @@
-import { useEffect } from "react";
 import { Container } from 'semantic-ui-react';
 import NavBar from "./NavBar";
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/store';
 import { observer } from "mobx-react-lite";
+import HomePage from '../../features/home/HomePage';
+import { Route, Routes } from 'react-router-dom';
+import ActivityForm from '../../features/activities/form/ActivityForm';
 
 function App() {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial) return <LoadingComponent content="Loading app" />
-
   return (
     <>
       <NavBar />
       <Container style={{ marginTop: "5em" }}>
-        <ActivityDashboard />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/activities' element={<ActivityDashboard />} />
+          <Route path='/createActivity' element={<ActivityForm />} />
+        </Routes>
       </Container>
     </>
   );
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <App />
-  </StoreContext.Provider>
-  ,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StoreContext.Provider>,
   document.getElementById('root')
 );
 
