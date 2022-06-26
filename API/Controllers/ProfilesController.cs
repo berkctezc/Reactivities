f0d3c1e29610e9d604a604eed2e98c5f8aec12d9
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2022-06-26 17:09:08 +0300

    profile edit feature

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -9,6 +9,12 @@ public class ProfilesController : BaseApiController
     [HttpGet("{username}")]
     public async Task<IActionResult> GetProfile(string username)
     {
-        return HandleResult(await Mediator.Send(new Details.Query {Username = username}));
+        return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Edit(Edit.Command command)
+    {
+        return HandleResult(await Mediator.Send(command));
     }
 }
